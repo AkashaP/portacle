26c3e0c0c511edac6802085ac3d1bdce741d15e1
Akasha Peppermint <>
2022-04-04 21:41:04 +1000

    bump2

--- build/sbcl.sh
+++ build/sbcl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-readonly TAG=sbcl-2.2.0
+readonly TAG=sbcl-2.2.3
 readonly REPOSITORY=https://github.com/sbcl/sbcl.git
 readonly DISABLED_FEATURES=":largefile :sb-ldb"
-readonly ENABLED_FEATURES=":sb-thread :sb-core-compression"
+readonly ENABLED_FEATURES=":sb-thread :sb-core-compression :fancy :sb-xref-for-internals :sb-show-assem"
 
 ###
 
